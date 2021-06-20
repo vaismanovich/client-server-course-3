c0d62b171f2d80a380b99a75d2c520bfa3dd58f3
Vitaliy Vaisman <vitaliyvaisman@gmail.com>
2021-06-20 17:12:25 +0400

    initiate project

---
+++ client-server-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  client-server-3
+//
+//  Created by Vitaliy on 20/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
